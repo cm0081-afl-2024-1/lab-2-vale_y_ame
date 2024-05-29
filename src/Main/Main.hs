eccd184e36a0633c460b6257a67b3ff668560829
Amelia Hoyos <amelia327.hoyos@gmail.com>
2024-05-29 12:34:33 -0500

    Test Cases

--- src/Main/Main.hs
+++ src/Main/Main.hs
@@ -1,26 +1,113 @@
 module Main where
 
+-- Import necessary modules
 import LC
+import Test.HUnit
 
-printOriginalAndResult :: LambdaExpress -> Int -> IO ()
-printOriginalAndResult originalExpr n = do
-  putStrLn $ "Original expression: " ++ printLambdaExpress originalExpr
+-- Example lambda expression that normalizes in one try
+lambdaIdentity :: LambdaExpress
+lambdaIdentity =
+  App (Abs "x" (Var "x")) (Var "Otter")
 
-  case isNormalized $ reduceNTimes originalExpr n of
-    Right normalizedTerm -> do
-      putStrLn $ "Normalized expression: " ++ printLambdaExpress normalizedTerm
-    Left errorMessage ->
-      putStrLn errorMessage
+-- Example lambda expression that normalizes in tries
+lambdaBinaryFunction :: LambdaExpress
+lambdaBinaryFunction =
+  App
+    (App
+      (App
+        (Abs "f"
+          (Abs "x"
+            (Abs "y"
+              (App (App (Var "f") (Var "x")) (Var "y"))
+            )
+          )
+        )
+        (Abs "a"
+          (Abs "b"
+            (App (App (Var "+") (Var "a")) (Var "b"))
+          )
+        )
+      )
+      (Var "3")
+    )
+    (Var "4")
 
--- Addition function: \x -> \y -> x + y
-additionFunction :: LambdaExpress
-additionFunction = Abs "x" (Abs "y" (App (App (Var "+") (Var "x")) (Var "y")))
 
--- Application: (\x -> \y -> x + y) 3 4
-testCase2 :: LambdaExpress
-testCase2 = App (App additionFunction (Var "3")) (Var "4")
+-- Example lambda expression that doesn't normalize
+lambdaNoNormal :: LambdaExpress
+lambdaNoNormal =
+  App (Abs "x" (App (Var "x") (Var "x"))) (Abs "x" (App (Var "x") (Var "x")))
 
+testNormalization :: Test
+testNormalization =
+  TestList
+    [ TestCase $
+        assertEqual
+          "Test 1: lambdaIdentity with 1 step"
+          "Otter"
+          (isNormalized (tnf lambdaIdentity 1)),
+      TestCase $
+        assertEqual
+          "Test 2: lambdaIdentity with 3 steps"
+          "Otter"
+          (isNormalized (tnf lambdaIdentity 3)),
+      TestCase $
+        assertEqual
+          "Test 3: lambdaIdentity with 5 steps"
+          "Otter"
+          (isNormalized (tnf lambdaIdentity 5)),
+      TestCase $
+        assertEqual
+          "Test 4: lambdaBinaryFunction with 1 step"
+          ("No normalization found with resources provided: "
+          ++ "(lambda x.(lambda y.(lambda a.(lambda b.+ a b)) x y)) 3 4")
+          (isNormalized (tnf lambdaBinaryFunction 1)),
+      TestCase $
+        assertEqual
+          "Test 5: lambdaBinaryFunction with 3 steps"
+          ("No normalization found with resources provided: "
+          ++ "(lambda a.(lambda b.+ a b)) 3 4")
+          (isNormalized (tnf lambdaBinaryFunction 3)),
+      TestCase $
+        assertEqual
+          "Test 6: lambdaBinaryFunction with 5 steps"
+          "+ 3 4"
+          (isNormalized (tnf lambdaBinaryFunction 5)),
+      TestCase $
+        assertEqual
+          "Test 7: lambdaNoNormal with 1 step"
+          ("No normalization found with resources provided: "
+          ++ "(lambda x.x x) (lambda x.x x)")
+          (isNormalized (tnf lambdaNoNormal 1)),
+      TestCase $
+        assertEqual
+          "Test 8: lambdaNoNormal with 3 steps"
+          ("No normalization found with resources provided: "
+          ++ "(lambda x.x x) (lambda x.x x)")
+          (isNormalized (tnf lambdaNoNormal 3)),
+      TestCase $
+        assertEqual
+          "Test 9: lambdaNoNormal with 5 steps"
+          ("No normalization found with resources provided: "
+          ++ "(lambda x.x x) (lambda x.x x)")
+          (isNormalized (tnf lambdaNoNormal 5))
+    ]
 
+-- Function to check normalization for a list of terms
+checkNormalizationList :: LambdaExpress -> [Int] -> IO ()
+checkNormalizationList expr ns =
+  mapM_
+    ( \n -> do
+        printOriginalAndResult expr n
+        putStrLn ""
+    )
+    ns
+
+-- Main function to execute tests and checks
 main :: IO ()
 main = do
-  printOriginalAndResult testCase2 2
+  checkNormalizationList lambdaIdentity [1, 3, 5]
+  checkNormalizationList lambdaBinaryFunction [1, 3, 5]
+  checkNormalizationList lambdaNoNormal [1, 3, 5]
+  _ <- runTestTT $ TestList [testNormalization]
+  return ()
--- src/lib/LC.hs
+++ src/lib/LC.hs
@@ -1,35 +1,62 @@
 module LC where
 
+-- Define the data type for lambda calculus expressions
 data LambdaExpress
-  = Var String
-  | Abs String LambdaExpress
-  | App LambdaExpress LambdaExpress
+  = Var String -- Variable
+  | Abs String LambdaExpress -- Abstraction (λx.t)
+  | App LambdaExpress LambdaExpress -- Aplication (t1 t2)
   deriving (Eq, Show)
 
+-- Function to convert a LambdaExpress to a string representation
 printLambdaExpress :: LambdaExpress -> String
 printLambdaExpress (Var x)     = x
-printLambdaExpress (Abs x t)   = "(" ++ "lambda " ++ x ++ "." ++ printLambdaExpress t ++ ")"
-printLambdaExpress (App t1 t2) = printLambdaExpress t1 ++ " " ++ printLambdaExpress t2
+printLambdaExpress (Abs x t)   =
+  "(" ++ "lambda " ++ x ++ "." ++ printLambdaExpress t ++ ")"
+printLambdaExpress (App t1 t2) =
+  printLambdaExpress t1 ++ " " ++ printLambdaExpress t2
 
+-- Substitution function to replace variables in a LambdaExpress
 substitution :: LambdaExpress -> String -> LambdaExpress -> LambdaExpress
-substitution (Var y) x s       = if x == y then s else Var y
-substitution (Abs y t1) x s    = if x == y then Abs y t1 else Abs y (substitution t1 x s)
-substitution (App t1 t2) x s   = App (substitution t1 x s) (substitution t2 x s)
+substitution (Var y) x s       =
+  if x == y then s else Var y
+substitution (Abs y t1) x s    =
+  if x == y then Abs y t1 else Abs y (substitution t1 x s)
+substitution (App t1 t2) x s   =
+  App (substitution t1 x s) (substitution t2 x s)
 
+-- Reduction function to simplify LambdaExpress ONE TIME
 reduction :: LambdaExpress -> LambdaExpress
 reduction (App (Abs x t1) t2) = substitution t1 x t2
 reduction (App t1 t2)         = App (reduction t1) t2
 reduction (Abs x t)           = Abs x (reduction t)
 reduction t                   = t
 
--- Perform reduction n times
-reduceNTimes :: LambdaExpress -> Int -> LambdaExpress
-reduceNTimes expr 0 = expr
-reduceNTimes expr n = reduceNTimes (reduction expr) (n - 1)
+-- Perform reduction n times on a LambdaExpress
+tnf :: LambdaExpress -> Int -> LambdaExpress
+tnf expr 0 = expr
+tnf expr n = tnf (reduction expr) (n - 1)
 
-isNormalized :: LambdaExpress -> Either String LambdaExpress
-isNormalized term =
-  let reducedTerm = LC.reduction term
-  in if reducedTerm == term
-       then Right term -- Term is in normal form
-       else Left "Normalization not possible with the resources provided"
+-- Check if a LambdaExpress is in normal form
+isNormalForm :: LambdaExpress -> Bool
+isNormalForm (Var _)     = True
+isNormalForm (Abs _ t)   = isNormalForm t
+isNormalForm (App (Abs _ _) _) = False
+isNormalForm (App t1 t2) = isNormalForm t1 && isNormalForm t2
+
+-- If LambdaExpress is not in normal form,
+-- put a message before returning reduction
+isNormalized :: LambdaExpress -> String
+isNormalized t =
+  if isNormalForm t
+    then printLambdaExpress t
+    else
+      "No normalization found with resources provided: " ++ printLambdaExpress t
+
+-- Print the original expression,
+-- resources available, and its result after normalization
+printOriginalAndResult :: LambdaExpress -> Int -> IO ()
+printOriginalAndResult originalExpr n = do
+  putStrLn $ "Number of resources: " ++ show n
+  putStrLn $ "Original expression: " ++ printLambdaExpress originalExpr
+  let result = isNormalized (tnf originalExpr n)
+  putStrLn $ "Normalized expression: " ++ result
